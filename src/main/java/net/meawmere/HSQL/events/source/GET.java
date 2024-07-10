76d0c81dd715e58e9e24b41440f082f36aa5c2e1
meawmere <163195014+meawmere@users.noreply.github.com>
2024-07-10 21:06:19 +0300

    Update GET.java
    null

--- src/main/java/net/meawmere/HSQL/events/source/GET.java
+++ src/main/java/net/meawmere/HSQL/events/source/GET.java
@@ -1,4 +1,13 @@
 package net.meawmere.HSQL.events.source;
 
+import java.sql.ResultSet;
+
 public class GET {
+    public String execute;
+    public ResultSet output;
+
+    public GET(String execute, ResultSet output) {
+        this.execute = execute;
+        this.output = output;
+    }
 }
